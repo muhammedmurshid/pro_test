af0a378cc62d8683826d9202fa6cf378de66decf
Murshid <muhsink00000@gmail.com>
2024-06-25 13:13:54 +0530

    base

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Pro Test",
+    'version': "14.0.1.0",
+    'sequence': "0",
+    'depends': ['base', 'mail'],
+    'data': [
+
+
+    ],
+    'demo': [],
+    'summary': "pro_test",
+    'description': "this_is_my_app",
+    'installable': True,
+    'auto_install': False,
+    'license': "LGPL-3",
+    'application': True
+}
